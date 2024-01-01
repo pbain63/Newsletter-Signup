7ac0ba674ac10e805fd8fad119f0f3aa171334f2
pbain63 <prodipbain63@gmail.com>
2024-01-01 19:20:28 +0600

    Remove inspector from app page and logo,footer css styles from signup page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-const { url } = require("inspector");
+
 
 const app = express();
 
